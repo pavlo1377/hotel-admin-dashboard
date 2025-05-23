9e1a4e57f6b82f2f9396a7c26eeafb61b1289a7f
pavlo1377 <pavlo.antohiv@gmail.com>
2025-05-23 17:00:42 +0300

    useLocalStorage custom hook refactored to ts

--- src/hooks/useLocalStorageState.ts
+++ src/hooks/useLocalStorageState.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export function useLocalStorageState(initialState, key) {
+export const useLocalStorageState = (initialState: boolean, key: string) => {
   const [value, setValue] = useState(function () {
     const storedValue = localStorage.getItem(key);
     return storedValue ? JSON.parse(storedValue) : initialState;
@@ -14,4 +14,4 @@ export function useLocalStorageState(initialState, key) {
   );
 
   return [value, setValue];
-}
+};
